795d0b141e51d2190c27f76c798d7797108d4bbb
moljac <mcvjetko@holisticware.net>
2015-11-19 11:34:30 +0100

    Pages Demo

---
+++ Pages/DemoPages/DemoPages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DemoPages
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = 
+					//new NavigationPage(new PageFirst())
+					//new PageCarousel()
+					new PageTabbed();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
